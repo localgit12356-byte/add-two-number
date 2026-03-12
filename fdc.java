9d0c264e823394927c33620ffd5f5cfc83624972
unknown <aman@gmail.com>
2026-03-12 11:38:36 +0530

    print my name

--- fdc.java
+++ fdc.java
@@ -3,11 +3,14 @@ public class fdc {
 
 
 
-    public static void main (String[] args){
+    public static void main (String[] args) {
 
-int one = 34;
-int  two = 23;
-        System.out.println(one+two);
+        int one = 34;
+        int two = 23;
+        System.out.println(one + two);
+        String name = "man";
+        System.out.println(name);
     }
+
 
 }
