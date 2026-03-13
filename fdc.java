c3f71b30d2ab9a17258a130b71eaa50bc9b311ea
unknown <aman@gmail.com>
2026-03-13 11:23:25 +0530

    table

--- fdc.java
+++ fdc.java
@@ -14,6 +14,11 @@ public class fdc {
         System.out.println(dictator);
         for(int a=1;a<=10;a++){
             System.out.println(a);
+
+        }
+       int n=5;
+        for(int c=1;c<=10;c++){
+            System.out.println(n*c);
         }
 
 
